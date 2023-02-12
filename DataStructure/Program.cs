0eebcb499fa36e9928ca7f8a3e5086855435a16e
Dipakkamble <kambledipak700@gmail.com>
2023-02-12 18:50:17 +0530

    [Dipak] Added data structure project

---
+++ DataStructure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> ll=new LinkedList<int>();
+            ll.AddLast(30);
+            ll.AddLast(20);
+            ll.AddFirst(10);
+
+        }
+    }
+}
